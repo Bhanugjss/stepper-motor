8a268b7320a2b45f07cbdf1b09c003839b27f497
Bhanugjss <50614303+Bhanugjss@users.noreply.github.com>
2019-05-20 17:18:08 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,63 @@
+/*
+ * GccApplication1.c
+ *
+ * Created: 16-05-2019 15:25:53
+ * Author : G J S S BHANU
+ */ 
+#define F_CPU 16000000UL
+#include <avr/io.h>
+#include <util/delay.h>
+
+
+int main(void)
+{
+	DDRA=0X0F;/*Make PORTA lower pins as output*/
+	int period=2;
+    while (1) 
+    {
+		for(int i=0;i<50;i++)
+		{
+			PORTA=0X09;
+			_delay_ms(period);
+			PORTA=0X08;
+			_delay_ms(period);
+			PORTA=0X0C;
+			_delay_ms(period);
+			PORTA=0X06;
+			_delay_ms(period);
+			PORTA=0X04;
+			_delay_ms(period);
+			PORTA=0X03;
+			_delay_ms(period);
+			PORTA=0X02;
+			_delay_ms(period);
+			PORTA=0X01;
+			_delay_ms(period);
+    }
+	PORTA=0X09;
+	_delay_ms(period);
+	_delay_ms(1000);
+	for(int i=0;i<50;i++)
+	{
+		
+		PORTA=0X01;
+	 	_delay_ms(period);
+		PORTA=0X03;
+		_delay_ms(period);
+		PORTA=0X02;
+		_delay_ms(period);
+		PORTA=0X04;
+		_delay_ms(period);
+		PORTA=0X06;
+		_delay_ms(period);
+		PORTA=0X0C;
+		_delay_ms(period);
+		PORTA=0X08;
+		_delay_ms(period);
+		PORTA=0X09;
+		_delay_ms(period);
+		
+	}
+	}
+}
+
